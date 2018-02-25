dcd6a6290b08de4d8c0281d7c0a57d116e3aaad2
akaneti <aaronkaneti@gmail.com>
2018-02-24 23:17:08 -0600

    remaining amount auto-update

--- nuWallet.js
+++ nuWallet.js
@@ -1,10 +1,16 @@
 var budget=160;//global variable for variable, so when it is editted it is editted everywhere.
 var green;
 var loginId;
+var amountSpent;
+var remaining;
 function progress()
 {
 	document.getElementById("progress-tomake").style.backgroundColor="green";
 	document.getElementById("cancel-button").value="back";
+	amountSpent=document.getElementById("amount-spent").value;
+	remaining=document.getElementById("remaining").value;
+	remaining-=amountSpent;
+	document.getElementById('remaining').value='You have $'+remaining+' remaining';
 };
 
 function register()
