58d9f8669f312fafb8c96dd16b6bdfe963fcc16e
akaneti <aaronkaneti@gmail.com>
2018-02-25 02:12:34 -0600

    touch id prompt

--- nuWallet.js
+++ nuWallet.js
@@ -1,19 +1,15 @@
 var budget=160;//global variable for variable, so when it is editted it is editted everywhere.
-var green;
-var loginId;
-var amountSpent;
-var remaining;
+var amountSpent2=document.getElementById("amount-spent").innerHTML;
+var firstRem=document.getElementById("remaining").innerHTML;
+firstRem-=amountSpent2;
 function progress()
 {
 	document.getElementById("progress-tomake").style.backgroundColor="#00E065";
 	document.getElementById("cancel-button").value="back";
-	amountSpent=document.getElementById("amount-spent").value;
-	remaining=document.getElementById("remaining").value;
+	var amountSpent=document.getElementById("amount-spent").innerHTML;
+	var amSp=parseInt(amountSpent);
+	var remaining=document.getElementById("remaining").innerHTML;
+	var rem=parseFloat(remaining);
 	remaining-=amountSpent;
-	document.getElementById('remaining').value='You have $'+remaining+' remaining';
-};
-
-function register()
-{
-	loginId=document.getElementById("username").value;
+	document.getElementById('remaining').innerHTML=firstRem;
 };
