f092155afeaf8f2dc59165a336c9179285bb8466
akaneti <aaronkaneti@gmail.com>
2018-03-14 14:46:17 -0500

    Added functional account page, fixed the other account page. Added change password page.

--- nuWallet.js
+++ nuWallet.js
@@ -38,3 +38,27 @@ function progress2()
 	document.getElementById('cancel-button').style.marginLeft=85;
 	document.getElementById('cancel-button').innerHTML="Done";
 };
+function validateBankRegistrationFields() {
+	//initializes variables
+	x = document.getElementById("user-id").value;
+	y = document.getElementById("user-password").value;
+	placeholderX = document.getElementById("user-id").placeholder;
+	placeholderY = document.getElementById("user-password").placeholder;
+	// Checks to see if userId and Password Fields are not empty
+	if (((placeholderX == "User ID") || (placeholderX == "Please insert your ID")) && (x=="")) {
+		document.getElementById("user-id").placeholder = "Please insert your ID";
+		if ((placeholderY == "Password") || (placeholderY == "Please insert your password")){
+		document.getElementById("user-password").placeholder = "Please insert your password";
+		}
+	}
+	else if (((placeholderY == "Password") || (placeholderY == "Please insert your password")) && (y=="")){
+		document.getElementById("user-password").placeholder = "Please insert your password";
+		if ((placeholderX == "User ID") || (placeholderX == "Please insert your ID")) {
+		document.getElementById("user-id").placeholder = "Please insert your ID";
+		}
+	}
+	else {
+		window.location.href='accountPage.html';
+	}
+
+}
